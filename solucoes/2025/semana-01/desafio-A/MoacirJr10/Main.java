1a45147be15d0c376f45394c3c45a8057f48addd
Mírian Gomes <nataliamirian506@gmail.com>
2025-08-31 14:32:14 -0300

    Revert "Solução Desafio A (Semana 01) - MoacirJr10"

--- solucoes/2025/semana-01/desafio-A/MoacirJr10/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(CpfValidator.validarCPF("123.456.789-09"));
-        System.out.println(CpfValidator.validarCPF("12345678909"));
-        System.out.println(CpfValidator.validarCPF("111.111.111-11"));
-        System.out.println(CpfValidator.validarCPF("123.456.789-10"));
-    }
-}
