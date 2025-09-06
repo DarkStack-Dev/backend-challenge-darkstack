65fd111498da256c75f3536a1830ee7ed2f13ebe
Guilherme Bueno <guilhermebueno816@gmail.com>
2025-09-06 13:06:04 -0300

    feat: adiciona solução do desafio B

---
+++ solucoes/2025/semana-01/desafio-B/GuilhermeBueno192/data/livros.py
@@ -0,0 +1,5 @@
+LIVROS = [
+    {"id": 1, "titulo": "1984", "autor": "George Orwell", "ano": 1949, "genero": "Ficção Científica", "disponivel": True},
+    {"id": 2, "titulo": "O Sol é para todos", "autor": "Harper Lee", "ano": 1960, "genero": "Romance", "disponivel": False}
+]
+
